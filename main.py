b5ed55bf4532a866b836c758e3ff2a3cb006e44f
Billy Paulsen <bpswim90@gmail.com>
2017-10-06 10:22:30 -0500

    Created index.html and basic file structure.

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run()
